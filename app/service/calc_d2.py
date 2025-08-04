b093ad1308120a7e897fc4437c6b784de1f424a6
Kora-carlos <carlos.souza@korasaude.com.br>
2025-08-04 09:02:58 -0300

    Melhoria no service/calc_d2

--- app/service/calc_d2.py
+++ app/service/calc_d2.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 
 def get_filtered_dates(reference_date=None):
+    if reference_date and isinstance(reference_date, str):
+        reference_date = datetime.fromisoformat(reference_date)
+
     today = reference_date or datetime.today()
     weekday = today.weekday() 
     filtered_dates = []
@@ -11,19 +14,17 @@ def get_filtered_dates(reference_date=None):
     elif weekday == 1:  # Terça
         for delta in [4, 3, 2]: 
             filtered_dates.append(today - timedelta(days=delta))
-    elif weekday == 2: 
+    elif weekday == 2:  # Quarta
         target_date = today - timedelta(days=2)
         filtered_dates.append(target_date)
-
     elif weekday == 3:  # Quinta
         target_date = today - timedelta(days=2)
         filtered_dates.append(target_date)
-
     elif weekday == 4:  # Sexta
         target_date = today - timedelta(days=2)
         filtered_dates.append(target_date)
-
     else:
         return
     return [date.strftime("%Y-%m-%d") for date in filtered_dates]
 
+#get_filtered_dates("2025-08-05")
--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == "__main__": 
     port = int(os.getenv("FLASK_RUN_PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
